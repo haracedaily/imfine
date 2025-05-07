fb573cff3f5b6122959e5070ff0c09bb7df16218
DESKTOP-29VI7GO\user <nj7001@naver.com>
2025-05-07 12:21:07 +0900

    no message

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 let chart_json_obj = [{"id":'0',"value":75},{"id":'1',"value":20}];
+
+/* 요소 선택 */
+
 const $yAxisMax = document.querySelector("#chart-inner-y-axis-max");
 const $xAxisContext = document.querySelector("#chart-inner-x-axis");
 const $chartInnerLayer = document.querySelector("#chart-inner-layer");
@@ -10,6 +13,7 @@ const $addGroup = document.querySelector("#value-add-func-group");
 const $proTextarea = document.querySelector("#value-pro-edit-textarea");
 
 
+/* 이벤트 선언 */
 
     $idInput.addEventListener('keydown',e=>{
         isValidNum(e);
@@ -30,6 +34,7 @@ const $proTextarea = document.querySelector("#value-pro-edit-textarea");
     
     reset_chart();
 
+    /* 함수 선언 */
     
     function reset_chart(){
         $chartInnerLayer.innerHTML='';
@@ -103,37 +108,36 @@ const $proTextarea = document.querySelector("#value-pro-edit-textarea");
     }
 
 
-    function remove_obj(e){
-        // chart_json_obj=chart_json_obj.filter(el=>el.id!=id); 바로 적용x => apply 버튼 클릭시 적용
-        // 따로 reset btn 제공
-        e.target.parentElement.parentElement.remove();
-    }
 
-
-
-
+/* 숫자만 입력할 수 있게끔 제한 */
 function isValidNum(e){
-    console.log(e);
-    
     if(/\d/.test(e.key)||e.keyCode===8||e.keyCode===46){
       /*   if(chart_json_obj.find(el=>el.id==e.target.value+e.key)){
-            중복값
+            중복값 별도처리
         }; */
     }else{
         e.preventDefault();
     }
 }
 
-function checkform(e){
-    event.preventDefault();
-    let testForm = document.querySelector('#testForm');
-    let formData = new FormData(testForm);
-    console.log(testForm);
-    console.log(formData);
-    console.log([...formData.entries()]);
-    console.log(this);
+/* 테이블 삭제 btn 함수 */
+function remove_obj(e){
+    // chart_json_obj=chart_json_obj.filter(el=>el.id!=id); 바로 적용x => apply 버튼 클릭시 적용
+    // 따로 reset btn 제공
+    e.target.parentElement.parentElement.remove();
 }
 
+/* 테이블 apply 버튼 이벤트 */
+function tableApply(){
+    chart_json_obj=[];
+    [...$innerEditTbody.children].map(el=>{
+        chart_json_obj.push({id:String(el.children[0].textContent),value:el.children[1].children[0].value});
+    });
+    reset_chart();
+}
+
+
+/* id input값에 따른 btn 활성화 */
 function isValidId(e){
     $addBtn.setAttribute('disabled',true);
     if(!!chart_json_obj.find(el=>el.id==e.target.value)){
@@ -152,6 +156,7 @@ function isValidId(e){
     }
 }
 
+/* value input값에 따른 btn 활성화 */
 function ableBtn(e){
     $addBtn.setAttribute('disabled',true);
     if(e.target.value?.length>0&&$addGroup.dataset.text=='사용가능한 id입니다.'){
@@ -162,34 +167,37 @@ function ableBtn(e){
     }
 }
 
+/* 테이블 add버튼 함수 */
 function addFunc(e){
     chart_json_obj.push({id:String($idInput.value),value:$valueInput.value});
     $idInput.value='';
     $valueInput.value='';
     $addGroup.dataset.text='';
+    /* 값 초기화에 따른 btn 비활성화 */
+    e.target.setAttribute('disabled',true);
 }
 
-function tableApply(){
-    chart_json_obj=[];
-    [...$innerEditTbody.children].map(el=>{
-        chart_json_obj.push({id:String(el.children[0].textContent),value:el.children[1].children[0].value});
-    });
-    reset_chart();
-}
-
+/* 고급 값 편집 apply 버튼 함수 */
 function proApply(e){
-    console.log(e);
-    console.log($proTextarea.value.replaceAll(JSON.stringify(chart_json_obj),''));
     try{
-        chart_json_obj=JSON.parse($proTextarea.value);
+        /* 혹여나 JSON.stringify 형태가 아닐 수 있기 때문에 try catch로 걸러준다. */
+        let data = JSON.parse($proTextarea.value).filter(el=>!!el.id)
+        chart_json_obj=[];
+        /* 중복 id가 있다면 제거 */
+        let ids = new Set();
+        data.forEach(element => {
+            if(!ids.has(String(element.id))){
+                /* Boolean에서 0==false라서 문자열 처리 */
+                element.id=String(element.id);
+                ids.add(element.id);
+                chart_json_obj.push(element);
+            }
+        });
+        $proTextarea.style.color="black";
         reset_chart();
     }catch{
-        $proTextarea.backgroundColor='red';
-        $proTextarea.parentElement.dataset.pro = "입력양식을 맞춰주세요.";
+        $proTextarea.style.color="red";
+        $proTextarea.parentElement.parentElement.dataset.pro = "입력양식을 맞춰주세요.";
     }
     
-}
-
-function proEditKeydown(e){
- 
 }
